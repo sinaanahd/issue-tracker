d0c40ef57243e325d9ff418abd9dcce6b6de6bba
sinananhid <sina.anahid@outlook.com>
2024-03-02 14:43:23 +0330

    style the active commit in the navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,11 +1,13 @@
+'use client';
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa";
-
+import classNames from 'classnames';
 
 
 const Navbar = () => {
-
+  const currentPath = usePathname();
   const links = [
     {
         id : 1,
@@ -18,7 +20,6 @@ const Navbar = () => {
         href : "/issues"
     }
   ]
-
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/">
@@ -27,10 +28,16 @@ const Navbar = () => {
         <ul className='flex space-x-6'>
             {
                 links.map(l => 
-                <li>
-                    <Link
-                        key={l.id}
-                        className='text-zinc-500 hover:text-zinc-800 transition-colors'
+                <li key={l.id}>
+                    <Link 
+                        // className={`text-zinc-${currentPath === l.href ? 900 : 500} hover:text-zinc-800 transition-colors`}
+                        className={classNames({
+                            "text-zinc-900" : l.href === currentPath,
+                            "text-zinc-500" : l.href !== currentPath,
+                            "font-bold" : l.href === currentPath,
+                            "hover:text-zinc-800" : true,
+                            "transition-colors" : true
+                        })}
                         href={l.href}
                         >
                         {l.label}
