3625aaddda0f3efcc8a9610fedf5cd6be2d18dee
sinananhid <sina.anahid@outlook.com>
2024-03-14 14:39:59 +0330

    adding issue edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,7 +4,9 @@ import { notFound } from 'next/navigation'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
 import styles from './issueDetails.module.css'
 import ReactMarkdown from 'react-markdown'
-import delay from 'delay'
+import { FiEdit } from 'react-icons/fi'
+import { Button } from '@radix-ui/themes'
+import Link from 'next/link'
 interface Props {
   params: { id: string }
 }
@@ -16,17 +18,25 @@ const IssueDetailsPage = async ({ params }: Props) => {
   if (!issue) {
     notFound()
   }
-  // await delay(2000)
   return (
     <div className={styles.pageContainer}>
-      <h1 className={styles.title}>{issue.title}</h1>
-      <div className={styles.textStatusWrapper}>
-        <IssueStatusBadge status={issue.status} />
-        <p className={styles.dateWrapper}>{issue.createdAT.toDateString()}</p>
+      <div className={styles.issueOldDatas}>
+        <h1 className={styles.title}>{issue.title}</h1>
+        <div className={styles.textStatusWrapper}>
+          <IssueStatusBadge status={issue.status} />
+          <p className={styles.dateWrapper}>{issue.createdAT.toDateString()}</p>
+        </div>
+        <ReactMarkdown className={styles.mainTexts}>
+          {issue.description}
+        </ReactMarkdown>
       </div>
-      <ReactMarkdown className={styles.mainTexts}>
-        {issue.description}
-      </ReactMarkdown>
+      <div className="second-col">
+        <Button>
+          <Link href={`/issues/${issue.id}/edit`}>
+            Edit Issue <FiEdit />
+          </Link>
+        </Button>
+      </div>
     </div>
   )
 }
