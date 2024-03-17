78beedce9351dd1dfe4d790647efe003b06fc3ee
sinananhid <sina.anahid@outlook.com>
2024-03-17 16:14:18 +0330

    handle delete issue errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -8,6 +8,7 @@ import { FaRegTrashAlt } from 'react-icons/fa'
 
 const DeleteIssueButton = ({ issueId }: { issueId: Number }) => {
   const [is_submitting, set_is_submitting] = useState(false)
+  const [error, set_error] = useState(false)
   const router = useRouter()
   const handle_delete_issue = async () => {
     try {
@@ -17,41 +18,61 @@ const DeleteIssueButton = ({ issueId }: { issueId: Number }) => {
       router.refresh()
     } catch (error) {
       set_is_submitting(false)
-      console.log('An unexpected error occured.')
+      set_error(true)
     }
   }
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">
-          Delete Issue
-          <FaRegTrashAlt />
-        </Button>
-      </AlertDialog.Trigger>
-      <AlertDialog.Content>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description>
-          Are you sure you want to delete this issue this action is undone!
-        </AlertDialog.Description>
-        <Flex className="mt-4" gap="3">
-          <AlertDialog.Cancel>
-            <Button color="gray" variant="soft">
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              color="red"
-              onClick={() => {
-                handle_delete_issue()
-              }}
-            >
-              Delete issue {is_submitting && <Spinner />}
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">
+            Delete Issue
+            <FaRegTrashAlt />
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue this action is undone!
+          </AlertDialog.Description>
+          <Flex className="mt-4" gap="3">
+            <AlertDialog.Cancel>
+              <Button color="gray" variant="soft">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button
+                color="red"
+                onClick={() => {
+                  handle_delete_issue()
+                }}
+              >
+                Delete issue {is_submitting && <Spinner />}
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error happend</AlertDialog.Title>
+          <AlertDialog.Description>
+            This issue can not be done because of an unexpected error
+          </AlertDialog.Description>
+          <Button
+            style={{ marginTop: '1rem' }}
+            color="gray"
+            variant="soft"
+            onClick={() => {
+              set_error(false)
+            }}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   )
 }
 
