8af0507591cb6bafc33fd254f56f841dd129b79b
sinananhid <sina.anahid@outlook.com>
2024-04-29 13:19:34 +0330

    making user model / getting users for select box from back end / making users get api

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,8 +1,18 @@
 'use client'
+import { User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 
 const AssigneeSelect = () => {
+  const [users, set_users] = useState<User[]>([])
+  const fetchUsers = async () => {
+    const { data } = await axios.get<User[]>('/api/users')
+    set_users(data)
+  }
+  useEffect(() => {
+    fetchUsers()
+  }, [])
   return (
     <div>
       <Select.Root>
@@ -10,7 +20,11 @@ const AssigneeSelect = () => {
         <Select.Content>
           <Select.Group>
             <Select.Label>Suggestions</Select.Label>
-            <Select.Item value="1">Sina Anahid</Select.Item>
+            {users.map((user) => (
+              <Select.Item value={user.id + ''} key={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
           </Select.Group>
         </Select.Content>
       </Select.Root>
