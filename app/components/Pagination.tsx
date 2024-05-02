ccec692e23c46dbf07c8afc77451482e237745f5
sinananhid <sina.anahid@outlook.com>
2024-05-02 16:22:42 +0330

    pagination completed

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,6 @@
+'use client'
 import { Button, Flex, Text } from '@radix-ui/themes'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 
 interface Props {
@@ -8,26 +10,51 @@ interface Props {
 }
 
 const Pagination = ({ itemCount, pageSize, currentPage }: Props) => {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+
   const pageCount = Math.ceil(itemCount / pageSize)
   if (pageCount <= 1) return null
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(searchParams)
+    params.set('page', page.toString())
+    router.push('?' + params.toString())
+  }
   return (
     <Flex align="center" gap="2">
       <Text>
         Page {currentPage} of {pageCount}
       </Text>
-      <Button variant="soft" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         {' '}
         {'<<'}{' '}
       </Button>
-      <Button variant="soft" disabled={currentPage === 1}>
+      <Button
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         {' '}
         {'<'}{' '}
       </Button>
-      <Button variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(currentPage + 1)}
+      >
         {' '}
         {'>'}{' '}
       </Button>
-      <Button variant="soft" disabled={currentPage === pageCount}>
+      <Button
+        variant="soft"
+        disabled={currentPage === pageCount}
+        onClick={() => changePage(pageCount)}
+      >
         {' '}
         {'>>'}{' '}
       </Button>
